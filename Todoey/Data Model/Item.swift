ef0eaa902a089c02a26f08ba8cde7ff6d8cd6d48
vishalvernekar <55388348+vishalvernekar@users.noreply.github.com>
2019-09-30 13:50:12 +0530

    Convert to  MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Vishal Vernekar on 30/09/19.
+//  Copyright © 2019 Vishal Vernekar. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
